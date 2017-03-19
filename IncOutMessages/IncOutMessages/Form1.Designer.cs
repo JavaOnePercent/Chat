94577a097e524d48e0c1fec295cfc615e0a23993
JavaOnePercent <vol12vol@mail.ru>
2017-03-19 15:59:48 +0500

    Add files via upload

---
+++ IncOutMessages/IncOutMessages/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace IncOutMessages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.comboBoxUser = new System.Windows.Forms.ComboBox();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.buttonPick = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSend.Location = new System.Drawing.Point(697, 103);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 21);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // comboBoxUser
+            // 
+            this.comboBoxUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBoxUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxUser.FormattingEnabled = true;
+            this.comboBoxUser.Items.AddRange(new object[] {
+            "Denis228 (Denis Vasjanovich)",
+            "Sanya1337 (Alexander Nosov)",
+            "Vova322 (Władimir Jakowlew)"});
+            this.comboBoxUser.Location = new System.Drawing.Point(12, 76);
+            this.comboBoxUser.Name = "comboBoxUser";
+            this.comboBoxUser.Size = new System.Drawing.Size(214, 21);
+            this.comboBoxUser.TabIndex = 2;
+            this.comboBoxUser.SelectedIndexChanged += new System.EventHandler(this.comboBoxUser_SelectedIndexChanged);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.BackColor = System.Drawing.SystemColors.Control;
+            this.labelMessage.Location = new System.Drawing.Point(244, 127);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(528, 425);
+            this.labelMessage.TabIndex = 3;
+            // 
+            // buttonPick
+            // 
+            this.buttonPick.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonPick.Location = new System.Drawing.Point(12, 12);
+            this.buttonPick.Name = "buttonPick";
+            this.buttonPick.Size = new System.Drawing.Size(214, 45);
+            this.buttonPick.TabIndex = 4;
+            this.buttonPick.Text = "Открыть Диалог";
+            this.buttonPick.UseVisualStyleBackColor = true;
+            this.buttonPick.Click += new System.EventHandler(this.buttonPick_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(247, 12);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(525, 85);
+            this.textBoxMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonPick);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.comboBoxUser);
+            this.Controls.Add(this.buttonSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ComboBox comboBoxUser;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button buttonPick;
+        private System.Windows.Forms.TextBox textBoxMessage;
+    }
+}
+
---
+++ IncOutMessages/IncOutMessages/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IncOutMessages
+{
+    public partial class Form1 : Form
+    {
+        //private string login = "Denis228";
+        //private string name = "Denis Vasjanovich";
+        private string login = "Vova322";
+        private string name = "Władimir Jakowlew";
+        private string comboBoxChat = "";
+        private string message = "";
+        private string file = "";
+        private string readText = "";
+        private bool buttonClick = false;
+        //private bool first = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (comboBoxUser.SelectedItem == null || buttonClick == false || string.IsNullOrEmpty(textBoxMessage.Text.Trim()))
+                return;
+            file = @"C:\Users\Владимир\Documents\Visual Studio 2015\Projects\IncOutMessages\IncOutMessages\Chats\" + comboBoxChat + "-" + login + " (" + name + ")" + ".txt";
+            if (!(File.Exists(file)))
+                file = @"C:\Users\Владимир\Documents\Visual Studio 2015\Projects\IncOutMessages\IncOutMessages\Chats\" + login + " (" + name + ")" + "-" + comboBoxChat + ".txt";
+            /*if (first == false)
+            {
+                message = login + " (" + name + ") : " + textBoxMessage.Text;
+                first = true;
+            }
+            else*/
+            message = login + " (" + name + ") : " + textBoxMessage.Text + Environment.NewLine;
+            File.AppendAllText(file, message);
+            readText = File.ReadAllText(file);
+            labelMessage.Text = readText;
+            textBoxMessage.Text = null;
+        }
+
+        private void comboBoxUser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxChat = comboBoxUser.SelectedItem.ToString();
+            buttonClick = false;
+
+    }
+
+        private void buttonPick_Click(object sender, EventArgs e)
+        {
+            if (comboBoxUser.SelectedItem == null)
+                return;
+            file = @"C:\Users\Владимир\Documents\Visual Studio 2015\Projects\IncOutMessages\IncOutMessages\Chats\" + comboBoxChat + "-" + login + " (" + name + ")" + ".txt";
+            if (!(File.Exists(file)))
+                file = @"C:\Users\Владимир\Documents\Visual Studio 2015\Projects\IncOutMessages\IncOutMessages\Chats\" + login + " (" + name + ")" + "-" + comboBoxChat + ".txt";
+            message = "";
+            File.AppendAllText(file, message);
+            readText = File.ReadAllText(file);
+            labelMessage.Text = readText;
+            buttonClick = true;
+        }
+    }
+}
